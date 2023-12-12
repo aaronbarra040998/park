f9f49e53bf602ecbcc46eed6a2683f8120ea3325
Aaron A. Barra <121281535+aaronbarra040998@users.noreply.github.com>
2023-12-12 13:35:00 -0500

    park

---
+++ car_action.js
@@ -0,0 +1,66 @@
+// part1
+var originalSrc = '/img/car_g1.png';
+var newSrc = '/img/car_r.png';
+
+document.querySelector('.card_g10').addEventListener('click', function() {
+    if (this.src.endsWith(originalSrc)) {
+        this.src = newSrc;
+    } else {
+        this.src = originalSrc;
+    }
+});
+document.querySelector('.card_g11').addEventListener('click', function () {
+  if (this.src.endsWith(originalSrc)) {
+      this.src = newSrc;
+  } else {
+      this.src = originalSrc;
+  }
+});
+document.querySelector('.card_g12').addEventListener('click', function() {
+  if (this.src.endsWith(originalSrc)) {
+      this.src = newSrc;
+  } else {
+      this.src = originalSrc;
+  }
+});
+document.querySelector('.card_g13').addEventListener('click', function() {
+  if (this.src.endsWith(originalSrc)) {
+      this.src = newSrc;
+  } else {
+      this.src = originalSrc;
+  }
+});
+document.querySelector('.card_g14').addEventListener('click', function() {
+  if (this.src.endsWith(originalSrc)) {
+      this.src = newSrc;
+  } else {
+      this.src = originalSrc;
+  }
+});
+
+
+// part2
+var originalSrc = '/img/car_g1.png';
+var newSrc = '/img/car_r.png';
+
+document.querySelector('.card_g20').addEventListener('click', function() {
+    if (this.src.endsWith(originalSrc)) {
+        this.src = newSrc;
+    } else {
+        this.src = originalSrc;
+    }
+});
+document.querySelector('.card_g21').addEventListener('click', function () {
+  if (this.src.endsWith(originalSrc)) {
+      this.src = newSrc;
+  } else {
+      this.src = originalSrc;
+  }
+});
+document.querySelector('.card_g22').addEventListener('click', function() {
+  if (this.src.endsWith(originalSrc)) {
+      this.src = newSrc;
+  } else {
+      this.src = originalSrc;
+  }
+});
